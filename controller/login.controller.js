e240a23e563285e385550c972dc3f9d61a7ecf13
hoanganhtrans2 <hoanganhtrans23@gmail.com>
2020-10-14 16:29:41 +0700

    backup

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,35 +1,34 @@
 var AWS = require("aws-sdk");
-const express = require('express');
+const express = require("express");
 const app = express();
 
 AWS.config.update({
-    region: "us-east-2"
-  });
-var docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"});
-
+  region: "us-east-2",
+});
+var docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-2" });
 
 module.exports.login = (req, res) => {
-
-    const {id, password} = req.body;
-    console.log(req.body);
-    var params = {
-        TableName: 'user-zalo',
-        Key:{
-            "userid": id,
-        }
-    };   
-    docClient.get(params, function(err, data) {
-        if (err) {
-            res.status(400).send('Bad req')
-        } else {
-            if(data.Item.password===password)
-                res.json(data);
-            else
-                res.status(402).send('Sai tai khoan hoac mat khau');
-            
-        }
-    });
-}
-
-
-
+  const { id, password } = req.body;
+  console.log(req.body);
+  var params = {
+    TableName: "user-zalo",
+    Key: {
+      userid: id,
+    },
+  };
+  docClient.get(params, function (err, data) {
+    if (err) {
+      res.status(400).send("Bad req");
+    } else {
+      if (!isEmpty(data)) {
+        if (data.Item.password === password) res.json(data);
+        else res.status(402).send("Sai tai khoan hoac mat khau");
+      } else {
+        res.status(402).send("Sai tai khoan hoac mat khau");
+      }
+    }
+  });
+};
+const isEmpty = (v) => {
+  return Object.keys(v).length === 0;
+};
